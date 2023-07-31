04d21b89cb18aba5ed9d20a5d55cc149e762810a
João Carlos Borges <136731149+Joao-carlos-zimba@users.noreply.github.com>
2023-07-31 13:09:29 -0300

    Quarto commit
    Arrumando a seção adicionar tarefa.

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-$(document).ready(function() {
+    $(document).ready(function() {
     $('button').click(function() {
         $('form').slideDown();
-})
+    })
 
     $('form').on('submit', function(e) {
         e.preventDefault();
         const adicionaNovaTarefa = $('#nova-tarefa').val();
         const novaTarefa = $('<li>');
         $(`<li> = "${adicionaNovaTarefa}" </li>`).appendTo(novaTarefa);
-        $(novaTarefa).appendTo('li');
+    
+        $(novaTarefa).appendTo('ul');
+    })
+
+    $("ul").click (function() {
+        $('li').css('text-decoration', 'line-through');
     })
 })
