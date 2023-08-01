51712d2b778c185b76c0d29ea66b37496fcbe2ef
João Carlos Borges <136731149+Joao-carlos-zimba@users.noreply.github.com>
2023-08-01 20:25:44 -0300

    Quinto commit
    colocando o tachado na tarefa adicionada.

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-    $(document).ready(function() {
-    $('button').click(function() {
-        $('form').slideDown();
-    })
+$("form").on("submit", function (e) {
+    e.preventDefault();
+    const adicionaNovaTarefa = $("#nova-tarefa").val();
+    const novaTarefa = $("<li>");
+    $(`<li> = "${adicionaNovaTarefa}" </li>`).appendTo(novaTarefa);
 
-    $('form').on('submit', function(e) {
-        e.preventDefault();
-        const adicionaNovaTarefa = $('#nova-tarefa').val();
-        const novaTarefa = $('<li>');
-        $(`<li> = "${adicionaNovaTarefa}" </li>`).appendTo(novaTarefa);
-    
-        $(novaTarefa).appendTo('ul');
-    })
+    $(novaTarefa).appendTo("ul");
 
-    $("ul").click (function() {
-        $('li').css('text-decoration', 'line-through');
-    })
-})
+$("li").click(function () {
+    $(this).css('text-decoration', 'line-through');
+});
+});
