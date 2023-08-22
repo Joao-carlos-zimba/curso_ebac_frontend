edfb3eab75309ec8f4aaef580d3b2557e16af357
João Carlos Borges <136731149+Joao-carlos-zimba@users.noreply.github.com>
2023-08-22 18:31:39 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000',{
+        placeholder: '(00) 00000-0000'
+    });
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required:true
+            },
+            email: {
+                required:true,
+                email:true,
+            },
+            mensagem: {
+                required:true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira seu nome',
+            email: 'Por favor, insira seu email',
+            mensagem: 'Por favor, insira sua mensagem'
+        }
+    })
+});
